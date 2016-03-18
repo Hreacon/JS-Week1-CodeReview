cca0bd7b8266be90bf7f1752cd5bb6ab727cb713
Nicholas Jensen-Hay <NicholasJensenHay@gmail.com>
2016-03-18 08:18:00 -0700

    add optional description of repo

--- js/GitHubApi.js
+++ js/GitHubApi.js
@@ -8,7 +8,8 @@ exports.GetUserRepos = function(username) {
       $(".template .user h3").html(response[0].owner.login);
       for(var i = 0; i < response.length; i++)
       {
-        var html = '<li><a href="'+response[i].html_url+'">'+response[i].name+'</a>';
+        var html = '<li><a href="'+response[i].html_url+'">'+response[i].name+'</a>';  if(response[i].description.length > 0)
+            html += '<div> Description: ' +response[i].description+'</div>';
         html += '</li>';
 
         $(".template .repositories ul").append(html);
