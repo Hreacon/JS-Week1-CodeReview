4410fc3ce8a26b3c758b472ae389d4b123dd3768
Nicholas Jensen-Hay <NicholasJensenHay@gmail.com>
2016-03-18 07:08:49 -0700

    Add JS to test form, works

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,3 +1,11 @@
+var GitHubApi = require('./../js/GitHubApi.js');
+
 $(document).ready(function() {
-  
+  $("#githubNameForm").submit(function(event) {
+    event.preventDefault();
+    var username = $("input[name=name]").val();
+    $("input[name=name]").val('');
+    $(".template .user h3").text(username);
+    $(".response").append($(".template").html());
+  });
 });
