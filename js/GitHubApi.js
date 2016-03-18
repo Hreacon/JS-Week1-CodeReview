7204f64dc20bb5bd642a475feb0160842d8627f9
Nicholas Jensen-Hay <NicholasJensenHay@gmail.com>
2016-03-18 08:28:05 -0700

    add basic user info to view

--- js/GitHubApi.js
+++ js/GitHubApi.js
@@ -1,11 +1,27 @@
 var apiKey = require('./../.env').ghApiKey;
 var moment = require('moment');
 
+exports.GetUserInfo = function(username) {
+  $.get("https://api.github.com/users/"+username+"?access_token="+apiKey)
+    .then(function(response) {
+      console.log(response);
+      $("[username="+response.login+"] .userInfo img").attr('src', response.avatar_url);
+      $(".userInfo .email").text(response.email);
+      $(".userInfo .name").text(response.name);
+      $(".userInfo .repoCount").text(response.public_repos);
+      $(".userInfo .followers").text(response.following);
+      $(".userInfo .location").text(response.location);
+      $(".userInfo .createdOn").text(moment(response.created_at).format('D/M/Y'));
+    });
+};
+
 exports.GetUserRepos = function(username) {
   $.get('https://api.github.com/users/'+username+'/repos?per_page=1000&access_token=' + apiKey)
     .then(function(response){
       console.log(response);
       $(".template .user h3").html(response[0].owner.login);
+      $(".template .user").attr("username", response[0].owner.login);
+      exports.GetUserInfo(username);
       for(var i = 0; i < response.length; i++)
       {
         var html = '<li><a href="'+response[i].html_url+'">'+response[i].name+'</a>  <div> Created On: ' + moment(response[i].created_at).format("M/D/Y") + ' </div>';
